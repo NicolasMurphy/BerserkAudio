3f2a4014e6c7a6ebe7eccb89c96973d88156c023
Nicolas Murphy <murphynick10@gmail.com>
2026-08-22 12:47:53 -0500

    add Combverb to README and sort modules alphabetically

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -7,9 +7,9 @@ Plugin* pluginInstance;
 void init(Plugin* p) {
 	pluginInstance = p;
 
+	p->addModel(modelCombverb);
 	p->addModel(modelScrambler);
 	p->addModel(modelXenizer);
-	p->addModel(modelCombverb);
 #ifndef METAMODULE
 	p->addModel(modelXenScribe);
 #endif
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -8,9 +8,9 @@ using namespace rack;
 extern Plugin* pluginInstance;
 
 // Declare each Model, defined in each module source file
+extern Model* modelCombverb;
 extern Model* modelScrambler;
 extern Model* modelXenizer;
-extern Model* modelCombverb;
 #ifndef METAMODULE
 extern Model* modelXenScribe;
 #endif
